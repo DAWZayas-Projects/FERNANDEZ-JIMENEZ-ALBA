bb3250a5ebeebae0ec52bc97b90bea627538c85d
bitTal <albafjez@gmail.com>
2016-06-15 16:51:48 +0200

    Provincias & municipios rows prepared to be intruduced into the database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+	require "#{Dir.pwd}/app/utils/localities.rb"
+	require "#{Dir.pwd}/app/utils/provinces.rb"
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  if Provincias.all.length == 0
+  	Provincias.create(provinces())
+  end
+
+  if Municipios.all.length == 0
+  	localities().each do |locality|
+  		Municipios.create(id_provincia: locality[0], cod_municipio: locality[1], DC: locality[2], nombre: locality[3] )
+  	end 
+  end
 end
